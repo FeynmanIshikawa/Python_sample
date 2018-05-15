0143dbca935b908cc6ed5bafe4c90de81db9a253
Ishikawa Yuta <36562303+FeynmanIshikawa@users.noreply.github.com>
2018-05-16 08:26:50 +0900

    Add files via upload

---
+++ Make.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class CopyLower {
+	
+
+int i;
+int a;
+
+int i = a;
+int i + a;
+Scanner sc = new Scanner(System.in);
+String line = sc.nextLine();
+
+if (line = 6) {
+	System.out.println("perfect"):
+} else if (line = 3 or line = 6) {
+	System.out.println("nearly");
+} else {
+	System.out.println("neither");
+}
+
+if (perfectNumber(num
+
+static boolean perfectNumber(int n) {
+	int sum = 0;
+	for (int i=0; i<= num; i++) {
+		
+	if (isPerfectNumber(n)) {
+		System.out.println("perfect");
+	else (
